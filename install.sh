5f0a5a18a1d911d473cbf402c966096c5afaa58d
Matthias Kay <github@matthiaskay.de>
2021-11-10 20:20:28 +0100

    fix: Bugs during installation (#5)
    * Use correct shebang
    
    * Fix order of install commands. Curl was not available
    
    * Make environment variables available
    
    * Replace SERVER_NAME by EXTERNAL_HOST_NAME
    
    * Move files into target/ before manipulating them. Otherwise they might be checked in by accident.
    
    Co-authored-by: kayma <kayma@hlag.com>

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
-!/bin/bash
+#!env bash
 set -euo pipefail
 
 # install Docker
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # install Docker Compose
@@ -17,26 +19,34 @@ if [ ! -f /usr/local/bin/docker-compose ]; then
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 
-if [ ! -d letsencrypt ]; then
-  mkdir -p letsencrypt/{conf,html}
-  # to be able to store the initially created self signed certificate
-  mkdir -p letsencrypt/conf/live/${EXTERNAL_HOST_NAME}
+source .env
+
+if [ ! -d target ]; then
+  mkdir -p target/nginx
 fi
 
-if [ ! -d mumble-server-data ]; then
-  mkdir mumble-server-data
+cp -dpr .env docker-compose.yml mumble-server mumble-web nginx target/
+
+if [ ! -d target/letsencrypt ]; then
+  mkdir -p target/letsencrypt/{conf,html}
+  # to be able to store the initially created self signed certificate
+  mkdir -p target/letsencrypt/conf/live/${EXTERNAL_HOST_NAME}
+fi
+
+if [ ! -d target/mumble-server-data ]; then
+  mkdir -p target/mumble-server-data
 fi
 
 # generate DH Params for Nginx
-if [ ! -f nginx/dhparams.pem ]; then
-  openssl dhparam -out nginx/dhparams.pem 2048
+if [ ! -f target/nginx/dhparams.pem ]; then
+  openssl dhparam -out target/nginx/dhparams.pem 2048
 fi
 
 # adjust the certificate path for mumble server
 SSL_CERTIFICATE_FILE="\\/etc\\/letsencrypt\\/live\\/${CERTIFICATE_DIRECTORY}\\/fullchain.pem"
 SSL_PRIVATE_KEY_FILE="\\/etc\\/letsencrypt\\/live\\/${CERTIFICATE_DIRECTORY}\\/privkey.pem"
 
-sed -i "s/^sslCert=.*/sslCert=${SSL_CERTIFICATE_FILE}/" mumble-server/murmur.ini
-sed -i "s/^sslKey=.*/sslKey=${SSL_PRIVATE_KEY_FILE}/" mumble-server/murmur.ini
+sed -i "s/^sslCert=.*/sslCert=${SSL_CERTIFICATE_FILE}/" target/mumble-server/murmur.ini
+sed -i "s/^sslKey=.*/sslKey=${SSL_PRIVATE_KEY_FILE}/" target/mumble-server/murmur.ini
 
-docker-compose up -d
+docker-compose -f target/docker-compose.yml up -d
