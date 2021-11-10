9fe9a2c5afd6eaec055248a5137cc754911293b0
kayma <kayma@hlag.com>
2021-11-10 13:59:31 +0100

    Move files into target/ before manipulating them. Otherwise they might be checked in by accident.

--- install.sh
+++ install.sh
@@ -21,26 +21,32 @@ fi
 
 source .env
 
-if [ ! -d letsencrypt ]; then
-  mkdir -p letsencrypt/{conf,html}
-  # to be able to store the initially created self signed certificate
-  mkdir -p letsencrypt/conf/live/${EXTERNAL_HOST_NAME}
+if [ ! -d target ]; then
+  mkdir -p target/nginx
 fi
 
-if [ ! -d mumble-server-data ]; then
-  mkdir mumble-server-data
+cp -dpr .env docker-compose.yml mumble-server mumble-web nginx target/
+
+if [ ! -d target/letsencrypt ]; then
+  mkdir -p target/letsencrypt/{conf,html}
+  # to be able to store the initially created self signed certificate
+  mkdir -p target/letsencrypt/conf/live/${EXTERNAL_HOST_NAME}
+fi
+
+if [ ! -d target/mumble-server-data ]; then
+  mkdir -p target/mumble-server-data
 fi
 
 # generate DH Params for Nginx
-if [ ! -f nginx/dhparams.pem ]; then
-  openssl dhparam -out nginx/dhparams.pem 2048
+if [ ! -f target/nginx/dhparams.pem ]; then
+  openssl dhparam -out target/nginx/dhparams.pem 2048
 fi
 
 # adjust the certificate path for mumble server
 SSL_CERTIFICATE_FILE="\\/etc\\/letsencrypt\\/live\\/${CERTIFICATE_DIRECTORY}\\/fullchain.pem"
 SSL_PRIVATE_KEY_FILE="\\/etc\\/letsencrypt\\/live\\/${CERTIFICATE_DIRECTORY}\\/privkey.pem"
 
-sed -i "s/^sslCert=.*/sslCert=${SSL_CERTIFICATE_FILE}/" mumble-server/murmur.ini
-sed -i "s/^sslKey=.*/sslKey=${SSL_PRIVATE_KEY_FILE}/" mumble-server/murmur.ini
+sed -i "s/^sslCert=.*/sslCert=${SSL_CERTIFICATE_FILE}/" target/mumble-server/murmur.ini
+sed -i "s/^sslKey=.*/sslKey=${SSL_PRIVATE_KEY_FILE}/" target/mumble-server/murmur.ini
 
-docker-compose up -d
+docker-compose -f target/docker-compose.yml up -d
